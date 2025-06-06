4e0ec24e7870d4ff628240c95e700001d54b7e8b
Saravanan M <saravananm@Saravanans-MacBook-Air.local>
2025-06-06 13:26:33 +0530

    adding node js files

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// MySQL Connection
+const db = mysql.createConnection({
+  host: '34.131.54.73',
+  user: 'root',
+  password: 'SSaamm#2357',
+  database: 'sys'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('DB connection failed:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+
+// Routes
+app.get('/api/users', (req, res) => {
+  db.query('SELECT * FROM MyTestDb.users',(err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(results);
+  });
+});
+
+app.post('/api/users', (req, res) => {
+  const { id, name, email, mobile } = req.body;
+  db.query('INSERT INTO MyTestDb.users (id, name, email, mobile) VALUES (?, ?, ?, ?)', [id, name, email, mobile], (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json({ id, name, email ,mobile});
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
